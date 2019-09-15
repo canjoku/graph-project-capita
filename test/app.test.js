b28f63fcf7b35ec2bdef3187b8098adfda8478b6
tony <tony@apples-mbp.home>
2019-09-15 12:06:50 +0100

    imported component to be tested

---
+++ test/app.test.js
@@ -0,0 +1,6 @@
+import sum from './instantiate-graph-test'
+
+
+test ('adding two nums', () => {
+  expect(sum(3,7)).toBe(10)
+})
---
+++ test/instantiate-graph-test.js
@@ -0,0 +1,9 @@
+import React from 'react'
+import { shallow } from 'enzyme'
+
+import App from '../src/components/App'
+import { italic } from 'ansi-colors';
+
+it('it should render without errors', () => {
+  
+})
